7bad7097a28323f9758b6bcf432fa7119a1d84ba
aleung18 <ashton.leung18@gmail.com>
2024-06-03 20:33:59 -0400

    update 2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,7 +13,7 @@ const Contact = () => {
       </div>
 
       <div className="font-plunct text-white flex flex-col justify-center items-center">
-        <form id="contact-form" name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" className="inline-flex flex-wrap justify-between w-full">
+        <form id="contact-form" name="contact" {...{ "netlify": true }} method="POST" netlify-honeypot="bot-field" className="inline-flex flex-wrap justify-between w-full">
           <input type="hidden" name="form-name" value="contact"/>
           <p className="hidden">
             <label>
